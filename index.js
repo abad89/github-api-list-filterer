505d60f61b2eedd214b1dd0d159ad41957a53a55
abad89 <abadalamenti89@gmail.com>
2021-04-19 23:51:13 -0400

    add filter for no auth button

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-const getApiList = () => {
-    return fetch(`https://api.publicapis.org/entries`)
-    .then(r => r.json())
-}
-
-const addApiToTbody = (api) => {
-    const tbody = document.getElementById(`api-table-body`)
-    tbody.innerHTML += `<tr><td><a href="${api.Link}">${api.API}</a></td><td>${api.Description}</td><td>${api.Category}</td><td>${api.Auth}</td><td>${api.Cors}</td><td>${api.HTTPS}</td></tr>`
-}
-
-getApiList().then(list => {
-    list.entries.forEach(api => addApiToTbody(api))
-    // console.log(list.entries)
-})
 //list.entries[x] = {
 //     "API": "Cat Facts",
 //     "Description": "Daily cat facts",
@@ -22,4 +8,40 @@ getApiList().then(list => {
 //     "Category": "Animals"
 // }
 
-// console.log(getApiList())
+let fullApiList = []
+let workingApiList = []
+const tbody = document.getElementById(`api-table-body`)
+const noAuthBtn = document.getElementById('no-auth-button')
+
+const getApiList = () => {
+    return fetch(`https://api.publicapis.org/entries`)
+    .then(r => r.json())
+}
+
+
+
+const addApiToTbody = (api) => {
+    tbody.innerHTML += `<tr><td><a href="${api.Link}">${api.API}</a></td><td>${api.Description}</td><td>${api.Category}</td><td>${api.Auth}</td><td>${api.Cors}</td><td>${api.HTTPS}</td></tr>`
+}
+
+const filterByNoAuth = (arr) => {
+    arr.forEach((api) => {
+        if (api.Auth === "") {
+            workingApiList.push(api)
+        }
+    })
+    workingApiList.forEach(api => addApiToTbody(api))
+}
+
+noAuthBtn.addEventListener("click", (e) => {
+    tbody.innerHTML = ""
+    filterByNoAuth(fullApiList)
+})
+
+
+getApiList().then(list => {
+    list.entries.forEach(api => addApiToTbody(api))
+    list.entries.forEach(api => fullApiList.push(api))
+    // console.log(fullApiList)
+})
+
